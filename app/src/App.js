862daaa47106ae8041f60cc55c42570b304b929d
Anirban <canirban99@gmail.com>
2019-02-18 02:35:15 +0100

    Frontend code restructure

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import Home from './Home';
+import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import GroupList from './GroupList';
+import GroupList from './components/GroupList';
 
 class App extends Component {
   render() {
--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../App.css';
 import AppNavbar from './AppNavbar';
 import { Link } from 'react-router-dom';
 import { Button, Container } from 'reactstrap';
